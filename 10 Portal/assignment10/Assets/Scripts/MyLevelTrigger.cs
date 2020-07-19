866506339a3a37b7213a7668a22ce1e5f3a2d647
mibnrashid <50634513+mibnrashid@users.noreply.github.com>
2020-07-19 20:18:43 +0300

    Upload 10
    Uploading Project num 10

---
+++ 10 Portal/assignment10/Assets/Scripts/MyLevelTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyLevelTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Destroy(this.gameObject);
+	}
+}
---
+++ 10 Portal/assignment10/Assets/Scripts/TextScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour {
+
+	public GameObject empty;
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+		text.color = new Color(0, 0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (empty != null) {
+			
+		}
+		else {
+			text.color = new Color(0, 0, 1, 1);
+			text.text = "You Won!!!";
+		}
+	}
+}
